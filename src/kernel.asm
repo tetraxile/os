499a7d24bf1902bf2f77f8c456837860db1cda3b
aster <tetraxile@gmail.com>
2023-06-16 22:16:53 +0100

    add hex/binary printing functions

--- src/kernel.asm
+++ src/kernel.asm
@@ -9,6 +9,27 @@ kernel_main:
     mov eax, strings.entered_kernel
     call print_string
 
+    mov al, 0x55
+    call print_u8_binary
+
+    mov al, 0xa
+    call print_char
+
+    mov al, 0xcd
+    call print_u8_hex
+
+    mov al, 0xa
+    call print_char
+
+    mov ax, 0xdead
+    call print_u16_hex
+
+    mov al, 0xa
+    call print_char
+    
+    mov eax, 0x12345678
+    call print_u32_hex
+
     hlt
 
 strings:
--- src/print.asm
+++ src/print.asm
@@ -4,6 +4,9 @@
 print_char:
     push edi
 
+    cmp al, 0xa
+    je .newline
+
     mov cl, al
 
     ; calculate cursor offset in VGA text buffer
@@ -25,7 +28,7 @@ print_char:
     cmp byte [cursor_x], 80
     jl .end
 
-    ; update cursor vertical position
+.newline:
     mov byte [cursor_x], 0
     inc byte [cursor_y]
 
@@ -45,20 +48,147 @@ print_string:
     cmp byte [esi], 0
     je .end
 
-    cmp byte [esi], 0xa
-    je .newline
-
     mov al, byte [esi]
     call print_char
     inc esi
     jmp .loop
 
-.newline:
-    mov byte [cursor_x], 0
-    inc byte [cursor_y]
-
 .end:
     pop esi
+    ret
+
+
+; write an unsigned 8-bit integer to the VGA text buffer
+; in hexadecimal at the cursor and update the cursor
+; * al - unsigned 8-bit integer
+print_u8_hex:
+    mov bl, al
+    mov cl, 4
+
+.loop:
+    mov al, bl
+    shr al, cl
+    and al, 0x0f
+
+    cmp al, 0x0a
+    jge .alpha
+
+    add al, '0'
+    jmp .print
+
+.alpha:
+    add al, 'a' - 0xa
+
+.print:
+    push ecx
+    call print_char
+    pop ecx
+
+    cmp cl, 0
+    jz .end
+    sub cl, 4
+    jmp .loop
+
+.end:
+    ret
+
+
+; write an unsigned 8-bit integer to the VGA text buffer
+; in binary at the cursor and update the cursor
+; * al - unsigned 8-bit integer
+print_u8_binary:
+    mov bl, al
+    mov cl, 7
+
+.loop:
+    mov al, bl
+    shr al, cl
+    and al, 0x01
+
+    add al, '0'
+    jmp .print
+
+.print:
+    push ecx
+    call print_char
+    pop ecx
+
+    cmp cl, 0
+    jz .end
+    sub cl, 1
+    jmp .loop
+
+.end:
+    ret
+
+
+; write an unsigned 16-bit integer to the VGA text buffer
+; in hexadecimal at the cursor and update the cursor
+; * ax - unsigned 16-bit integer
+print_u16_hex:
+    mov bx, ax
+    mov cl, 12
+
+.loop:
+    mov ax, bx
+    shr ax, cl
+    and ax, 0x0f
+
+    cmp ax, 0x0a
+    jge .alpha
+
+    add ax, '0'
+    jmp .print
+
+.alpha:
+    add ax, 'a' - 0xa
+
+.print:
+    push ecx
+    call print_char
+    pop ecx
+
+    cmp cl, 0
+    jz .end
+    sub cl, 4
+    jmp .loop
+
+.end:
+    ret
+
+
+; write an unsigned 32-bit integer to the VGA text buffer
+; in hexadecimal at the cursor and update the cursor
+; * eax - unsigned 32-bit integer
+print_u32_hex:
+    mov ebx, eax
+    mov cl, 28
+
+.loop:
+    mov eax, ebx
+    shr eax, cl
+    and eax, 0x0f
+
+    cmp eax, 0x0a
+    jge .alpha
+
+    add eax, '0'
+    jmp .print
+
+.alpha:
+    add eax, 'a' - 0xa
+
+.print:
+    push ecx
+    call print_char
+    pop ecx
+
+    cmp cl, 0
+    jz .end
+    sub cl, 4
+    jmp .loop
+
+.end:
     ret
 
 
