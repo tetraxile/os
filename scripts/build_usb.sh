92581d4dfc22847438f57cd1c420ed51ae8179eb
aster <tetraxile@gmail.com>
2023-08-03 18:10:04 +0100

    remove specific block device

--- scripts/build_usb.sh
+++ scripts/build_usb.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 
-OUT_DISK=/dev/sda
+OUT_DEVICE=$1
 
-if [ -f "$OUT_DISK" ]; then
-    echo "error: disk $OUT_DISK not found" >&2
+if [ -z "$1" ]; then
+    echo "error: no disk entered" >&2
+    exit 1
+elif [ ! -b "$1" ]; then
+    echo "error: device $OUT_DEVICE not found" >&2
     exit 1
 fi
 
 scripts/build.sh &&
-sudo dd if=build/os.img of=$OUT_DISK 2> /dev/null &&
-sudo eject /dev/sda
+sudo dd if=build/os.img of="$OUT_DEVICE" 2> /dev/null &&
+sudo eject "$OUT_DEVICE"
