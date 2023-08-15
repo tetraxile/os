cb2a7702d103960dabfadd61917385157cc04202
aster <tetraxile@gmail.com>
2023-08-15 02:09:10 +0100

    include boot stage 2 file

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,6 +2,5 @@
 
 mkdir -p build &&
 fasm src/boot.asm build/boot.bin > /dev/null &&
-fasm src/boot_stage_2.asm build/boot_stage_2.bin > /dev/null &&
 fasm src/kernel.asm build/kernel.bin > /dev/null &&
-cat build/boot.bin build/boot_stage_2.bin build/kernel.bin > build/os.img
+cat build/boot.bin build/kernel.bin > build/os.img
