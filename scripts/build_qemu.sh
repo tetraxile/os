9d94fc8aef34e0c47ef95dda87f99ca25fd5d10c
tetraxile <tetraxile@proton.me>
2026-01-28 00:10:52 +0100

    wow a commit

--- scripts/build_qemu.sh
+++ scripts/build_qemu.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
+# -device isa-debug-exit,iobase=0xf4,iosize=0x04
+#     lets you exit the emulator from the os by writing a value to I/O port 0xf4 (actual exit code is `input * 2 + 1`)
+
 scripts/build.sh &&
-qemu-system-x86_64 -drive format=raw,file=build/os.img
+qemu-system-x86_64 -drive format=raw,file=build/os.img -device isa-debug-exit,iobase=0xf4,iosize=0x04
